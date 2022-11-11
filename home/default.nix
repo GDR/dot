d030c06f612d4a607f05635361fe036bdad1aae0
Damir Garifullin <gosugdr@gmail.com>
2022-11-11 10:08:50 +0000

    Add awesomewm theme

--- home/default.nix
+++ home/default.nix
@@ -35,10 +35,6 @@
     };
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.alacritty.enable = true;
