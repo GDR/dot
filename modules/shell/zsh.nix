48636357ea07a0d13d184516c4fc4a6ca1a700a9
Damir Garifullin <gosugdr@gmail.com>
2022-11-16 20:39:23 +0300

    Add history autocomplete for zsh
    Apparently oh-my-zsh has been controlling
    this fancy feature that helped me for many
    years to autocomplete prompt by prefix.

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -12,9 +12,21 @@ in {
   config = mkIf cfg.enable {
     user.shell = pkgs.zsh;
 
+    environment.sessionVariables = {
+      HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE = "1";
+    };
+
+    # environment.pathsToLink = "/share/zsh";
+
     home.programs.zsh = {
       enable = true;
       enableCompletion = true;
+      enableAutosuggestions = true;
+
+      oh-my-zsh = {
+        enable = true;
+      };
+
       zplug = {
         enable = true;
         plugins = [
@@ -23,7 +35,6 @@ in {
           { name = "chisui/zsh-nix-shell"; tags = ["depth:1"]; }
         ];
       };
-
 
       initExtra = ''
         source ~/.p10k.zsh
