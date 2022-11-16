2c1a95ad4e086d1bf0650f7d85c96587c6c009b2
Damir Garifullin <gosugdr@gmail.com>
2022-11-16 16:58:09 +0000

    Change zsh plugins manager to zplug

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -10,42 +10,29 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs = {
-      zsh = {
-        enable = true;
-        enableCompletion = true;
-        shellAliases = {
-          "ls" = "ls -l --color --group-directories-first";
-          "dotfiles" = "cd ~/.config/nixos";
-        };
-        # plugins = [];
-        ohMyZsh = {
-          enable = true;
-          plugins = [ 
-            "git"
-
-          ];
-          theme = "agnoster";
-        };
-      };
-    };
-
     user.shell = pkgs.zsh;
 
     home.programs.zsh = {
-      plugins = [
-        "git"
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.5.0";
-            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
-          };
-        }
-      ];
+      enable = true;
+      enableCompletion = true;
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "plugins/git"; tags = ["from:oh-my-zsh"]; }
+          { name = "romkatv/powerlevel10k"; tags = ["as:theme" "depth:1"]; }
+          { name = "chisui/zsh-nix-shell"; tags = ["depth:1"]; }
+        ];
+      };
+
+
+      initExtra = ''
+        source ~/.p10k.zsh
+      '';
+
+      shellAliases = {
+        "ls" = "exa -l --group-directories-first";
+        "dotfiles" = "cd ~/.config/nixos";
+      };
     };
   };
 }
