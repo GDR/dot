0b2f1829eefb5e2469f3451d3f7d3035d10d7983
Damir Garifullin <gosugdr@gmail.com>
2023-03-08 19:44:42 +0300

    feat: Change hardware configuration to work with disk labels

--- hosts/nix-germany/hardware-configuration.nix
+++ hosts/nix-germany/hardware-configuration.nix
@@ -14,17 +14,17 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/92f86285-274f-4d5a-a2b4-0d5982d577ca";
+    { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/FA45-9963";
+    { device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/b291df97-52bb-4438-8ccf-210c108f357c"; }
+    [ { device = "/dev/disk/by-label/swap"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
