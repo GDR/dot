35504a175c21361ed22adf675089f743855ea8e7
Damir Garifullin <gosugdr@gmail.com>
2024-03-04 23:52:09 +0300

    chore: Remove redundant code from flake.nix

--- flake.nix
+++ flake.nix
@@ -23,67 +23,6 @@
         config = { allowUnfree = true; };
       };
 
-      configuration = { pkgs, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment.systemPackages =
-          with pkgs; [
-            nixpkgs-fmt
-          ];
-
-        modules = {
-          common = {
-            devtools = {
-              direnv.enable = true;
-              git.enable = true;
-            };
-            shell = {
-              zsh.enable = true;
-            };
-            editors = {
-              neovim.enable = true;
-            };
-          };
-          workstation = {
-            fonts.enable = true;
-            editor = {
-              # vscode.enable = true;
-            };
-            terminal = {
-              kitty.enable = true;
-            };
-            osx = {
-              enable = true;
-            };
-          };
-        };
-
-        nix.configureBuildUsers = true;
-
-        # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
-        # nix.package = pkgs.nix;
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        # programs.zsh.enable = true; # default shell on catalina
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        security.pam.enableSudoTouchIdAuth = true;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 4;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
-
       lib = nixpkgs.lib.extend (lib: _: {
         my = import ./lib { inherit inputs lib; };
       });
