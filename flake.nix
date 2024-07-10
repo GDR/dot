f7807d3c383729ab0a653cfcb5a997968e5f46f3
Damir Garifullin <gosugdr@gmail.com>
2024-07-10 09:44:47 +0300

    chore: Remove redundant spaces in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,12 +3,10 @@
         nixpkgs = {
             url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         };
-
         nix-darwin = {
             url = "github:LnL7/nix-darwin";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
