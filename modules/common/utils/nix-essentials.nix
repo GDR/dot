f258ccdbb94650cf03f6eeb5b63038c3303d4256
Damir Garifullin <gosugdr@gmail.com>
2024-07-17 00:21:37 +0300

    feat: Add nixpkgs-fmt by default

---
+++ modules/common/utils/nix-essentials.nix
@@ -0,0 +1,17 @@
+{ config, options, pkgs, lib, ... }: with lib;
+let 
+  cfg = config.modules.common.utils.keepassxc; 
+in {
+  options.modules.common.utils.keepassxc = with types; {
+    enable = mkOption {
+      default = true;
+      type = types.bool;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      nixpkgs-fmt
+    ];
+  };
+}
