ccdad1fbafb73238b3f3df284ac47630c08f03bc
Damir Garifullin <gosugdr@gmail.com>
2024-07-20 15:08:05 +0300

    chore: Fix styling

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,11 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 
-{ pkgs ? (import ../nixpkgs.nix) { }, system, ... }: let 
+{ pkgs ? (import ../nixpkgs.nix) { }, system, ... }:
+let
   isDarwin = system == "x86_64-darwin" || system == "aarch64-darwin";
-in{
+in
+{
   apple-emoji-ttf = pkgs.callPackage ./apple-emoji-ttf { };
 } // pkgs.lib.optionalAttrs isDarwin {
   vfkit = pkgs.callPackage ./vfkit { };
