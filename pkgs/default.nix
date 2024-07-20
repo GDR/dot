2e44b50ece20ab7c9bd8c1861393d8afa5144e1c
Damir Garifullin <gosugdr@gmail.com>
2024-07-20 17:28:31 +0300

    chore: Add pkgs for linux only

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,9 +4,12 @@
 { pkgs ? (import ../nixpkgs.nix) { }, system, ... }:
 let
   isDarwin = system == "x86_64-darwin" || system == "aarch64-darwin";
+  isLinux = system == system == "x86_64-linux";
 in
 {
   apple-emoji-ttf = pkgs.callPackage ./apple-emoji-ttf { };
 } // pkgs.lib.optionalAttrs isDarwin {
   vfkit = pkgs.callPackage ./vfkit { };
+} // pkgs.lib.optionalAttrs isLinux {
+  
 }
