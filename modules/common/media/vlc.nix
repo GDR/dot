83e7f2977768abad21df26c83f8b59363575f745
Damir Garifullin <gosugdr@gmail.com>
2024-07-22 21:44:26 +0300

    chore: Fix platform specification for vlc

--- modules/common/media/vlc.nix
+++ modules/common/media/vlc.nix
@@ -1,6 +1,8 @@
 { config, options, pkgs, lib, ... }: with lib;
 let
   cfg = config.modules.common.media.vlc;
+  isLinux = options.system.platform == "linux";
+  isDarwin = options.system.platform == "darwin";
 in
 {
   options.modules.common.media.vlc = with types; {
@@ -10,7 +12,9 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = { }
+    // mkIf (cfg.enable && isLinux) { }
+    // mkIf (cfg.enable && isDarwin) {
     homebrew.casks = [
       "vlc"
     ];
