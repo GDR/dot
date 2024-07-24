20203220c05e7d5e6f4a42eaa7473e8737a177b0
Damir Garifullin <gosugdr@gmail.com>
2024-07-24 22:07:37 +0300

    feat: Add shortcuts to neotree

--- modules/common/editors/neovim/dotfiles/plugins/neotree.nix
+++ modules/common/editors/neovim/dotfiles/plugins/neotree.nix
@@ -7,5 +7,29 @@ in {
         enable = true;
       };
     };
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>e";
+        action = "<cmd>Neotree toggle<cr>";
+        options.desc = "Toggle explorer";
+      }
+      {
+        mode = "n";
+        key = "<leader>o";
+        options.desc = "Toggle explorer focus";
+
+        # https://github.com/AstroNvim/AstroNvim/blob/v4.7.7/lua/astronvim/plugins/neo-tree.lua#L12-L18
+        action.__raw = ''
+          function()
+            if vim.bo.filetype == "neo-tree" then
+              vim.cmd.wincmd "p"
+            else
+              vim.cmd.Neotree "focus"
+            end
+          end
+        '';
+      }
+    ];
   };
 }
