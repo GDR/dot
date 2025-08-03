abfee68c028ead8c82d27abc2f5014f7f096edcb
Damir Garifullin <gosugdr@gmail.com>
2025-08-03 22:23:23 +0300

    feat: Changed default terminal to ghostty

--- modules/linux/awesomewm/dotfiles/keys.lua
+++ modules/linux/awesomewm/dotfiles/keys.lua
@@ -56,7 +56,7 @@ globalkeys = gears.table.join(
         {description = "go back", group = "client"}),
 
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.spawn("kitty") end,
+    awful.key({ modkey,           }, "Return", function () awful.spawn("ghostty") end,
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
