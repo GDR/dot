6fb9507dffecd8a0cd3252dfb1046d94c5ea71b2
Damir Garifullin <gosugdr@gmail.com>
2025-08-17 19:12:56 +0300

    feat: Fixed grub to support windows

--- hosts/nix-goldstar/default.nix
+++ hosts/nix-goldstar/default.nix
@@ -7,12 +7,13 @@
   ];
 
   boot.kernelPackages = pkgs.linuxPackages;
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.device = "nodev";
+  boot.loader.grub.gfxmodeEfi = "1280x1024x32,auto";
 
   networking.hostName = "nix-goldstar";
   networking.networkmanager.enable = true;
