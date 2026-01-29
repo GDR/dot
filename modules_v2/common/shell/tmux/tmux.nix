c87f59855958326ff4cee9e34a23b9446d2a8716
Damir Garifullin <gosugdr@gmail.com>
2026-01-29 10:36:14 +0300

    refactor(tmux): migrate to mkModuleV2 pattern
    - Replace manual meta/options/config boilerplate with mkModuleV2
    - Auto-route programs.tmux to enabled users via allSystems
    - Use integrated dotfiles parameter for symlinks
    - Reduce from 67 to 40 lines

--- modules_v2/common/shell/tmux/tmux.nix
+++ modules_v2/common/shell/tmux/tmux.nix
@@ -1,66 +1,39 @@
 # Tmux terminal multiplexer
-{ config, pkgs, lib, system, _modulePath, self, ... }: with lib;
-let
-  modulePath = _modulePath;
-  moduleTags = [ "shells" ];
-  enabledUsers = filterAttrs (_: u: u.enable) config.hostUsers;
-in
-{
-  meta = lib.my.mkModuleMeta {
-    tags = moduleTags;
-    platforms = [ "linux" "darwin" ];
-    description = "Tmux terminal multiplexer";
-  };
+{ lib, pkgs, ... }@args:
 
-  options = lib.my.mkModuleOptions modulePath {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
+lib.my.mkModuleV2 args {
+  tags = [ "shells" ];
+  description = "Tmux terminal multiplexer";
+
+  module = {
+    allSystems = {
+      programs.tmux = {
+        enable = true;
+        shell = "${pkgs.zsh}/bin/zsh";
+        terminal = "tmux-256color";
+        historyLimit = 10000;
+        keyMode = "vi";
+        mouse = true;
+        baseIndex = 1;
+        escapeTime = 0;
+
+        plugins = with pkgs.tmuxPlugins; [
+          sensible
+          yank
+          resurrect
+          continuum
+        ];
+
+        extraConfig = ''
+          # Source custom config from dotfiles
+          source-file ~/.config/tmux/tmux.conf
+        '';
+      };
     };
   };
 
-  config =
-    let
-      shouldEnable = lib.my.shouldEnableModule { inherit config modulePath moduleTags; };
-    in
-    mkIf shouldEnable (mkMerge [
-      # Tmux program configuration
-      {
-        home-manager.users = mapAttrs
-          (name: _: {
-            programs.tmux = {
-              enable = true;
-              shell = "${pkgs.zsh}/bin/zsh";
-              terminal = "tmux-256color";
-              historyLimit = 10000;
-              keyMode = "vi";
-              mouse = true;
-              baseIndex = 1;
-              escapeTime = 0;
-
-              plugins = with pkgs.tmuxPlugins; [
-                sensible
-                yank
-                resurrect
-                continuum
-              ];
-
-              extraConfig = ''
-                # Source custom config from dotfiles
-                source-file ~/.config/tmux/tmux.conf
-              '';
-            };
-          })
-          enabledUsers;
-      }
-
-      # Dotfiles symlink (live-editable)
-      {
-        home-manager.users = lib.my.mkDotfilesSymlink {
-          inherit config self;
-          path = "tmux";
-          source = "modules_v2/common/shell/tmux/dotfiles";
-        };
-      }
-    ]);
+  dotfiles = {
+    path = "tmux";
+    source = "modules_v2/common/shell/tmux/dotfiles";
+  };
 }
