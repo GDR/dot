d8250a69f98f74daf27fec69855ba71e28e8945a
Damir Garifullin <gosugdr@gmail.com>
2026-01-29 10:25:14 +0300

    refactor(bitwarden): simplify using mkModuleV2 pattern
    32 → 12 lines using new curried mkModuleV2 syntax

--- modules_v2/common/security/bitwarden.nix
+++ modules_v2/common/security/bitwarden.nix
@@ -1,31 +1,12 @@
 # Bitwarden - secure password manager
-# https://mynixos.com/nixpkgs/package/bitwarden-desktop
-{ config, pkgs, lib, system, _modulePath, ... }: with lib;
-let
-  mkModule = lib.my.mkModule system config;
-  modulePath = _modulePath;
-  moduleTags = [ "security" ];
-in
-{
-  meta = lib.my.mkModuleMeta {
-    tags = moduleTags;
-    platforms = [ "linux" "darwin" ];
-    description = "Bitwarden password manager";
-  };
+{ lib, pkgs, ... }@args:
 
-  options = lib.my.mkModuleOptions modulePath {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
+lib.my.mkModuleV2 args {
+  tags = [ "security" ];
+  description = "Bitwarden password manager";
+  module = {
+    allSystems = {
+      home.packages = [ pkgs.bitwarden-desktop ];
     };
   };
-
-  config =
-    let
-      shouldEnable = lib.my.shouldEnableModule { inherit config modulePath moduleTags; };
-    in
-    mkIf shouldEnable (mkModule {
-      nixosSystems.home.packages = [ pkgs.bitwarden-desktop ];
-      darwinSystems.homebrew.casks = [ "bitwarden" ];
-    });
 }
