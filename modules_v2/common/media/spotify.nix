5d6c795a276f20f9c7a562342dd68438906941a1
Damir Garifullin <gosugdr@gmail.com>
2026-01-30 09:14:00 +0300

    refactor: migrate spotify to mkModuleV2
    - Replace manual mkModule pattern with mkModuleV2 wrapper
    - Simplify module structure while preserving functionality
    - Maintain cross-platform support (NixOS packages + Darwin Homebrew)

--- modules_v2/common/media/spotify.nix
+++ modules_v2/common/media/spotify.nix
@@ -1,33 +1,12 @@
 # Spotify music streaming
-{ config, pkgs, lib, system, _modulePath, ... }: with lib;
-let
-  mkModule = lib.my.mkModule system config;
-  modulePath = _modulePath;
-  moduleTags = [ "media" ];
+{ lib, pkgs, ... }@args:
 
-  pathParts = splitString "." modulePath;
-  cfg = foldl (acc: part: acc.${part}) config.modules pathParts;
-in
-{
-  meta = lib.my.mkModuleMeta {
-    tags = moduleTags;
-    platforms = [ "linux" "darwin" ];
-    description = "Spotify music streaming client";
+lib.my.mkModuleV2 args {
+  tags = [ "media" ];
+  platforms = [ "linux" "darwin" ];
+  description = "Spotify music streaming client";
+  module = {
+    nixosSystems.home.packages = [ pkgs.spotify ];
+    darwinSystems.homebrew.casks = [ "spotify" ];
   };
-
-  options = lib.my.mkModuleOptions modulePath {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
-    };
-  };
-
-  config =
-    let
-      shouldEnable = lib.my.shouldEnableModule { inherit config modulePath moduleTags; };
-    in
-    mkIf shouldEnable (mkModule {
-      nixosSystems.home.packages = [ pkgs.spotify ];
-      darwinSystems.homebrew.casks = [ "spotify" ];
-    });
 }
