91ab0baa223245e3a27fe040845fc95406368169
Damir Garifullin <gosugdr@gmail.com>
2026-01-30 09:14:31 +0300

    refactor: migrate cursor theme desktop utils to mkModuleV2
    - Replace manual mkModule pattern with mkModuleV2 wrapper
    - Simplify module structure while preserving functionality
    - Linux-only module (whitesur-cursors, dconf)

--- modules_v2/common/desktop/cursor.nix
+++ modules_v2/common/desktop/cursor.nix
@@ -1,35 +1,14 @@
 # Cursor theme and settings
-{ config, pkgs, lib, system, _modulePath, ... }: with lib;
-let
-  mkModule = lib.my.mkModule system config;
-  modulePath = _modulePath;
-  moduleTags = [ "desktop-utils" ];
+{ lib, pkgs, ... }@args:
 
-  pathParts = splitString "." modulePath;
-  cfg = foldl (acc: part: acc.${part}) config.modules pathParts;
-in
-{
-  meta = lib.my.mkModuleMeta {
-    tags = moduleTags;
-    platforms = [ "linux" ];
-    description = "Cursor theme (WhiteSur) and dconf settings utility";
+lib.my.mkModuleV2 args {
+  tags = [ "desktop-utils" ];
+  platforms = [ "linux" ];
+  description = "Cursor theme (WhiteSur) and dconf settings utility";
+  module = {
+    nixosSystems.home.packages = with pkgs; [
+      whitesur-cursors
+      dconf
+    ];
   };
-
-  options = lib.my.mkModuleOptions modulePath {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
-    };
-  };
-
-  config =
-    let
-      shouldEnable = lib.my.shouldEnableModule { inherit config modulePath moduleTags; };
-    in
-    mkIf shouldEnable (mkModule {
-      nixosSystems.home.packages = with pkgs; [
-        whitesur-cursors
-        dconf
-      ];
-    });
 }
