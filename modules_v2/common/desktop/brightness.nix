5954cf9c2330b2e35883eaf6ceac5e0d624651e5
Damir Garifullin <gosugdr@gmail.com>
2026-01-30 09:14:26 +0300

    refactor: migrate brightness desktop utils to mkModuleV2
    - Replace manual mkModule pattern with mkModuleV2 wrapper
    - Simplify module structure while preserving functionality
    - Linux-only module (brightnessctl)

--- modules_v2/common/desktop/brightness.nix
+++ modules_v2/common/desktop/brightness.nix
@@ -1,32 +1,11 @@
 # Brightness control
-{ config, pkgs, lib, system, _modulePath, ... }: with lib;
-let
-  mkModule = lib.my.mkModule system config;
-  modulePath = _modulePath;
-  moduleTags = [ "desktop-utils" ];
+{ lib, pkgs, ... }@args:
 
-  pathParts = splitString "." modulePath;
-  cfg = foldl (acc: part: acc.${part}) config.modules pathParts;
-in
-{
-  meta = lib.my.mkModuleMeta {
-    tags = moduleTags;
-    platforms = [ "linux" ];
-    description = "Brightness control (brightnessctl)";
+lib.my.mkModuleV2 args {
+  tags = [ "desktop-utils" ];
+  platforms = [ "linux" ];
+  description = "Brightness control (brightnessctl)";
+  module = {
+    nixosSystems.home.packages = [ pkgs.brightnessctl ];
   };
-
-  options = lib.my.mkModuleOptions modulePath {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
-    };
-  };
-
-  config =
-    let
-      shouldEnable = lib.my.shouldEnableModule { inherit config modulePath moduleTags; };
-    in
-    mkIf shouldEnable (mkModule {
-      nixosSystems.home.packages = [ pkgs.brightnessctl ];
-    });
 }
