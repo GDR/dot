72702f47e8a4a087757e33579a2ff2e2d7f18e59
Damir Garifullin <gosugdr@gmail.com>
2026-01-30 09:14:16 +0300

    refactor: migrate qbittorrent to mkModuleV2
    - Replace manual mkModule pattern with mkModuleV2 wrapper
    - Simplify module structure while preserving functionality
    - Maintain cross-platform support (NixOS packages + Darwin Homebrew)

--- modules_v2/common/downloads/qbittorrent.nix
+++ modules_v2/common/downloads/qbittorrent.nix
@@ -1,29 +1,12 @@
 # qBittorrent torrent client
-{ config, pkgs, lib, system, _modulePath, ... }: with lib;
-let
-  mkModule = lib.my.mkModule system config;
-  modulePath = _modulePath;
-  moduleTags = [ "downloads" ];
+{ lib, pkgs, ... }@args:
 
-  pathParts = splitString "." modulePath;
-  cfg = foldl (acc: part: acc.${part}) config.modules pathParts;
-in
-{
-  meta = lib.my.mkModuleMeta {
-    tags = moduleTags;
-    platforms = [ "linux" "darwin" ];
-    description = "qBittorrent torrent client";
-  };
-
-  options = lib.my.mkModuleOptions modulePath {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
-    };
-  };
-
-  config = mkIf cfg.enable (mkModule {
+lib.my.mkModuleV2 args {
+  tags = [ "downloads" ];
+  platforms = [ "linux" "darwin" ];
+  description = "qBittorrent torrent client";
+  module = {
     nixosSystems.home.packages = [ pkgs.qbittorrent ];
     darwinSystems.homebrew.casks = [ "qbittorrent" ];
-  });
+  };
 }
