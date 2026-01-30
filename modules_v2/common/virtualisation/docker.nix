9189ec5f872140add441cdfd6c6cd2d9fe3cd2a8
Damir Garifullin <gosugdr@gmail.com>
2026-01-30 09:15:53 +0300

    refactor: migrate docker to mkModuleV2
    - Replace manual module pattern with mkModuleV2 wrapper
    - Use darwinSystems for macOS docker CLI packages
    - Preserve system-level Linux config (virtualisation.docker, users.users) via merge
    - Simplify user detection using enabled hostUsers
    - Maintain cross-platform support

--- modules_v2/common/virtualisation/docker.nix
+++ modules_v2/common/virtualisation/docker.nix
@@ -1,59 +1,37 @@
 # Docker container runtime
-{ config, pkgs, lib, system, _modulePath, ... }: with lib;
+{ lib, pkgs, config, system, ... }@args: with lib;
 let
-  modulePath = _modulePath;
-  moduleTags = [ "oci-containers" ];
-
   isDarwin = pkgs.stdenv.isDarwin;
   isLinux = pkgs.stdenv.isLinux;
-
-  # Get users who have this module enabled via tag
-  enabledUsers = lib.my.getUsersWithModule { inherit config modulePath moduleTags; };
+  enabledUsers = filterAttrs (_: u: u.enable) (config.hostUsers or { });
   enabledUsernames = attrNames enabledUsers;
-  hasEnabledUsers = enabledUsernames != [ ];
-in
-{
-  meta = lib.my.mkModuleMeta {
-    tags = moduleTags;
+
+  baseModule = lib.my.mkModuleV2 args {
+    tags = [ "oci-containers" ];
     platforms = [ "linux" "darwin" ];
     description = "Docker container runtime";
-  };
-
-  options = lib.my.mkModuleOptions modulePath {
-    enable = mkOption {
-      default = false;
-      type = types.bool;
+    module = {
+      darwinSystems.home.packages = with pkgs; [
+        docker
+        docker-credential-helpers
+      ];
     };
   };
+in
+baseModule // {
+  config = mkMerge [
+    baseModule.config
+    # Linux: Enable docker daemon system-wide, add users to docker group
+    (mkIf isLinux {
+      virtualisation.docker.enable = true;
 
-  config =
-    let
-      shouldEnable = lib.my.shouldEnableModule { inherit config modulePath moduleTags; };
-    in
-    mkIf shouldEnable (mkMerge [
-      # Linux: Enable docker daemon system-wide, add users to docker group
-      (mkIf isLinux {
-        virtualisation.docker.enable = true;
-
-        # Add all users with the tag to docker group
-        users.users = listToAttrs (map
-          (username: {
-            name = username;
-            value = { extraGroups = [ "docker" ]; };
-          })
-          enabledUsernames);
-      })
-
-      # Darwin: Install docker CLI packages for users
-      (mkIf isDarwin {
-        home-manager.users = mapAttrs
-          (name: _: {
-            home.packages = with pkgs; [
-              docker
-              docker-credential-helpers
-            ];
-          })
-          enabledUsers;
-      })
-    ]);
+      # Add all enabled users to docker group
+      users.users = listToAttrs (map
+        (username: {
+          name = username;
+          value = { extraGroups = [ "docker" ]; };
+        })
+        enabledUsernames);
+    })
+  ];
 }
