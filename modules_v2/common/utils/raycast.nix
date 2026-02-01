b8f760f0348f8f100b14d6dfeede4c3e9a2d4619
Damir Garifullin <gosugdr@gmail.com>
2026-02-01 12:15:20 +0300

    feat: migrate raycast to modules_v2, cleanup old darwin utils

---
+++ modules_v2/common/utils/raycast.nix
@@ -0,0 +1,14 @@
+# Raycast - Spotlight replacement and productivity launcher for macOS
+{ lib, pkgs, ... }@args:
+lib.my.mkModuleV2 args {
+  tags = [ "utils" "desktop-utils" ];
+  platforms = [ "darwin" ];  # macOS only
+  description = "Raycast launcher";
+
+  module = {
+    darwinSystems = {
+      home.packages = [ pkgs.raycast ];
+    };
+  };
+}
+
