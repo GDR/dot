422fbd38d6b28cca7fd1c5b10b6d7e2269e7cfd1
Damir Garifullin <gosugdr@gmail.com>
2026-02-11 23:19:44 +0300

    feat: Add overlay for updating cursor-code to the latests version
    - Added script from nixpkgs that updates code-cursor hashes
    - Added overlay for code-cursor

---
+++ overlays/code-cursor/code-cursor.nix
@@ -0,0 +1,34 @@
+{ lib, system, ... }:
+final: prev:
+let
+  inherit (prev.stdenv) hostPlatform;
+  sourcesJson = lib.importJSON ./sources.json;
+  sources = lib.mapAttrs
+    (
+      _: info:
+        prev.fetchurl {
+          inherit (info) url hash;
+        }
+    )
+    sourcesJson.sources;
+
+  source = sources.${hostPlatform.system};
+
+  pname = "code-cursor";
+  version = sourcesJson.version;
+  vscodeVersion = sourcesJson.vscodeVersion;
+in
+{
+  code-cursor = prev.code-cursor.overrideAttrs (_oldAttrs: {
+    inherit version vscodeVersion;
+    src =
+      if hostPlatform.isLinux then
+        prev.appimageTools.extract
+          {
+            inherit pname version;
+            src = source;
+          }
+      else
+        source;
+  });
+}
--- overlays/default.nix
+++ overlays/default.nix
@@ -9,18 +9,19 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
-    # Override Ollama to version 0.11.3
-    ollama = prev.ollama.overrideAttrs (oldAttrs: {
-      version = "0.11.3";
-      src = final.fetchFromGitHub {
-        owner = "ollama";
-        repo = "ollama";
-        rev = "v0.11.3";
-        hash = "sha256-FghgCtVQIxc9qB5vZZlblugk6HLnxoT8xanZK+N8qEc=";
-      };
-      vendorHash = "sha256-SlaDsu001TUW+t9WRp7LqxUSQSGDF1Lqu9M1bgILoX4=";
-    });
-
-  };
+  modifications = final: prev:
+    {
+      # Override Ollama to version 0.11.3
+      ollama = prev.ollama.overrideAttrs (_oldAttrs: {
+        version = "0.11.3";
+        src = final.fetchFromGitHub {
+          owner = "ollama";
+          repo = "ollama";
+          rev = "v0.11.3";
+          hash = "sha256-FghgCtVQIxc9qB5vZZlblugk6HLnxoT8xanZK+N8qEc=";
+        };
+        vendorHash = "sha256-SlaDsu001TUW+t9WRp7LqxUSQSGDF1Lqu9M1bgILoX4=";
+      });
+    }
+    // (import ./code-cursor/code-cursor.nix { lib = final.lib; system = system; } final prev);
 }
