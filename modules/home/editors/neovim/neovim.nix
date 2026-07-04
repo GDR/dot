55707847d0c4784592258b78d1a00bda419dcb6c
Damir Garifullin <gosugdr@gmail.com>
2026-07-04 20:54:15 +0300

    feature: Remove all plugins from neovim

--- modules/home/editors/neovim/neovim.nix
+++ modules/home/editors/neovim/neovim.nix
@@ -4,18 +4,7 @@
 lib.my.mkModuleV2 args {
   platforms = [ "linux" "darwin" ];
   description = "Neovim editor with nixvim configuration";
-  imports = [
-    ./dotfiles/colorschema.nix
-    ./dotfiles/general.nix
-    ./dotfiles/keymaps.nix
-    ./dotfiles/plugins/airline.nix
-    ./dotfiles/plugins/neoscroll.nix
-    ./dotfiles/plugins/neotree.nix
-    ./dotfiles/plugins/noice.nix
-    ./dotfiles/plugins/telescope.nix
-    ./dotfiles/plugins/which-key.nix
-    ./dotfiles/plugins/web-devicons.nix
-  ];
+
   systemModule = {
     # Enable nixvim (system-level NixOS option, not home-manager)
     # Must be wrapped in allSystems so mkModuleV2 unwraps it correctly
