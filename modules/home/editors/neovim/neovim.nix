a72100c2d505951683004069d92c092f127dd8ce
Damir Garifullin <gosugdr@gmail.com>
2026-07-05 13:59:07 +0300

    fix: Fix nix-oldstar nvim wrapper

--- modules/home/editors/neovim/neovim.nix
+++ modules/home/editors/neovim/neovim.nix
@@ -1,22 +1,18 @@
 # Neovim editor with nix-wrapper-modules (Lua-first config)
 # Plugins and LSPs are managed by Nix; configuration lives as standard Lua files
 # that can be edited and reloaded instantly without nixos-rebuild.
-{ lib, pkgs, inputs, system, overlays ? [], ... }@args:
+{ lib, pkgs, inputs, ... }@args:
 
 let
   # Import plugin/LSP declarations
   pluginConfig = import ./dotfiles/plugins.nix { inherit pkgs lib; };
 
-  # Build a fresh pkgs for the wrapper module to avoid NixOS module system
-  # pkgs type-check issues (nix-wrapper-modules expects raw nixpkgs)
-  wrapperPkgs = import inputs.nixpkgs {
-    inherit system;
-    config = { allowUnfree = true; };
-  };
-
   # Build a wrapped neovim with all plugins and runtime tools baked in
+  # NOTE: NixOS pkgs can have _type = "if" (from mkIf in nixpkgs.config),
+  # which causes nix-wrapper-modules to misinterpret pkgs as a conditional.
+  # Override _type back to "pkgs" to fix the type check.
   wrappedNeovim = inputs.nix-wrapper-modules.wrappers.neovim.wrap {
-    pkgs = wrapperPkgs;
+    pkgs = pkgs // { _type = "pkgs"; };
     inherit (pluginConfig) specs runtimePkgs;
   };
 in
