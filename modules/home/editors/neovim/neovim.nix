9d56746b85484b6a08cd3ef02499d6564c3ea9b2
Damir Garifullin <gosugdr@gmail.com>
2026-07-22 07:05:33 +0300

    fix(neovim): link nvim config directory from nvim-nix input
    - Set xdg.configFile."nvim".source to nvim-nix dotfiles path
    - Lock nvim-nix flake input to latest revision

--- modules/home/editors/neovim/neovim.nix
+++ modules/home/editors/neovim/neovim.nix
@@ -12,6 +12,7 @@ lib.my.mkModuleV2 args {
     # The wrapped neovim has plugins + LSPs on its PATH.
     # ripgrep/fzf/fd also added globally for shell usage.
     allSystems.home.packages = [ wrappedNeovim ] ++ (with pkgs; [ ripgrep fzf fd ]);
+    allSystems.xdg.configFile."nvim".source = "${inputs.nvim-nix}/dotfiles/nvim";
     # wl-clipboard is the Wayland clipboard provider; pbcopy/pbpaste are built-in on Darwin
     nixosSystems.home.packages = [ pkgs.wl-clipboard ];
   };
